4454e1ba48639ec819bd0f39eb7a38f6b03c7da2
Rafael Freitas <rafaelborges1985@hotmail.com>
2020-10-04 20:04:37 -0300

    Add a burguer icon and a side menu

---
+++ menu.js
@@ -0,0 +1,20 @@
+let menuIcon = document.querySelector(".menu__icon")
+let closeIcon = document.querySelector(".close__icon")
+let navbar = document.querySelector(".navbar__list")
+let navbarItems = document.querySelectorAll(".navbar__links")
+
+menuIcon.addEventListener("click", () => {
+  navbar.classList.add("open")
+}, false)
+
+closeIcon.addEventListener("click", () => {
+  navbar.classList.remove("open")
+}, false)
+
+navbarItems.forEach(item => {
+  item.addEventListener("click", () => {
+    if(navbar.classList.contains("open")){
+      navbar.classList.remove("open")
+    }
+  }, false)
+});
